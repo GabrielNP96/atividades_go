c61605d70fe8cf25af93b6fb41932e4c0d65a24d
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-02-13 18:28:23 +0000

    tipos

---
+++ 01_amador/02_tipos/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var word string = "palavra"
+	var numberInt int = 2025
+	var numberFloat float64 = 45.5
+
+	fmt.Println(word, numberInt, numberFloat)
+}
